90656cd8e115ba63c47e3c580c277d7214916c12
rogerio-stubs <rostubs17@gmail.com>
2023-03-30 00:14:16 -0300

    data: delete table offensive and add offensive with field on category

--- prisma/migrations/20230329031447_init/migration.sql
+++ prisma/migrations/20230329031447_init/migration.sql
@@ -26,7 +26,7 @@ CREATE TABLE "Category" (
 -- CreateTable
 CREATE TABLE "Todo" (
     "id" TEXT NOT NULL,
-    "category_id" TEXT NOT NULL,
+    "category_id" TEXT,
     "name" TEXT NOT NULL,
     "description" TEXT,
     "checked" BOOLEAN NOT NULL,
@@ -52,4 +52,4 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 ALTER TABLE "Category" ADD CONSTRAINT "Category_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Todo" ADD CONSTRAINT "Todo_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
